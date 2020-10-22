bf38fb3ea3194559310976e8618420982ef013b7
Андросов Вадим <73282658+Rose-Crabel@users.noreply.github.com>
2020-10-22 13:13:17 +0300

    Add files via upload

---
+++ ANDROSOV IDLE Python 2E.py
@@ -0,0 +1,13 @@
+#a=int(input("a="))
+#s=a%10
+#print(s)
+
+#n=int(input("n="))
+#S=(n%10)+(n//100)+(n//10)%10
+#print(S)
+
+a=int(input("a="))
+b=int(input("b="))
+c=int(input("c="))
+S=((a//2)+(a%2))+((b//2)+(b%2))+((c//2)+(c%2))
+print(S)
